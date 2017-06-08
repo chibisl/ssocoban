6ff772556025a9b5d7d51e26d09617a52293119a
chibisl <chibisl@ukr.net>
2017-06-08 16:39:12 +0300

    add assets

---
+++ android/assets/levels/level1.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="64" tileheight="64" tilecount="32" columns="16">
+ <image source="tiles.png" width="1024" height="128"/>
+ <tile id="17">
+  <objectgroup draworder="index"/>
+ </tile>
+</tileset>
---
+++ android/assets/levels/robot.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="rotot" tilewidth="64" tileheight="75" tilecount="1" columns="1">
+ <image source="1.png" width="64" height="75"/>
+</tileset>
