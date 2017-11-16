c8d81f66f172b603dc6bc476126e4a837eb2072b
chibisl <chibisl@ukr.net>
2017-11-17 01:09:22 +0200

    - add new graphics

---
+++ android/assets/levels/objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="objects" tilewidth="64" tileheight="96" tilecount="4" columns="4">
+ <image source="socoban_box.png" width="256" height="96"/>
+</tileset>
