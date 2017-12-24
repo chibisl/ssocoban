71c37600021fcda2630f47a397f13b2f3cc73d46
chibisl <chibisl@ukr.net>
2017-12-24 21:35:48 +0200

    - add exit animation

---
+++ android/assets/maps/objects.tsx
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="objects" tilewidth="64" tileheight="128" tilecount="64" columns="16">
+ <image source="objects.png" width="1024" height="512"/>
+ <tile id="16">
+  <animation>
+   <frame tileid="16" duration="400"/>
+   <frame tileid="17" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="18">
+  <animation>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="19" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="loop" type="bool" value="false"/>
+  </properties>
+  <animation>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="31" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="32">
+  <animation>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="33" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="34">
+  <animation>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="35" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="36">
+  <animation>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="37" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="38">
+  <animation>
+   <frame tileid="38" duration="100"/>
+   <frame tileid="39" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="loop" type="bool" value="false"/>
+  </properties>
+  <animation>
+   <frame tileid="48" duration="100"/>
+   <frame tileid="49" duration="100"/>
+   <frame tileid="50" duration="100"/>
+   <frame tileid="51" duration="100"/>
+   <frame tileid="52" duration="100"/>
+   <frame tileid="53" duration="100"/>
+   <frame tileid="54" duration="100"/>
+   <frame tileid="55" duration="100"/>
+   <frame tileid="56" duration="100"/>
+   <frame tileid="57" duration="100"/>
+   <frame tileid="58" duration="100"/>
+   <frame tileid="59" duration="100"/>
+   <frame tileid="60" duration="100"/>
+   <frame tileid="61" duration="100"/>
+   <frame tileid="62" duration="100"/>
+   <frame tileid="63" duration="100"/>
+   <frame tileid="52" duration="100"/>
+   <frame tileid="53" duration="100"/>
+   <frame tileid="54" duration="100"/>
+   <frame tileid="55" duration="100"/>
+   <frame tileid="56" duration="100"/>
+   <frame tileid="57" duration="100"/>
+   <frame tileid="58" duration="100"/>
+   <frame tileid="59" duration="100"/>
+   <frame tileid="60" duration="100"/>
+   <frame tileid="61" duration="100"/>
+   <frame tileid="62" duration="100"/>
+   <frame tileid="63" duration="100"/>
+   <frame tileid="52" duration="100"/>
+   <frame tileid="53" duration="100"/>
+   <frame tileid="54" duration="100"/>
+   <frame tileid="55" duration="100"/>
+   <frame tileid="56" duration="100"/>
+   <frame tileid="57" duration="100"/>
+   <frame tileid="58" duration="100"/>
+   <frame tileid="59" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
